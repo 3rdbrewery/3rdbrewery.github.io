2f00412660e59ce1b7cb08d49f88637b9a4e42eb
3rdbrewery <3rdbrewery@gmail.com>
2023-01-08 23:23:02 +0900

    commit

--- single_sketch.js
+++ single_sketch.js
@@ -1,31 +1,36 @@
 // Single-sketch example
 
-let b1;
-let b2;
-
 function setup (){
   createCanvas (windowWidth-20, windowHeight-20);
-  textSize(windowWidth/7.5); 
+  textSize(windowWidth/7);
+  myfont = loadFont('assets/kopub.otf');
+  textFont(myfont);
 }
 
 function draw(){
-  textAlign(LEFT,TOP);
+  background(255);
+  fill(0);
   noStroke();
-  text("제3양조", 0,0);
-  text("제3탁주", 0,windowWidth/7.5);
-  text("제3탁주12", 0,2*windowWidth/7.5);
-  text("제3과하주", 0,3*windowWidth/7.5);
-  
+  textAlign(LEFT, TOP);
+  text('제3양조', 0,0);
+  text('제3탁주', 0,200);
+  text('제3과하주', 0,400);
+  text('제3소주', 0,600);
+  text('3rd_brewery', 0,800);
+  showWinSize();
 }
 
 function windowResized() {
   resizeCanvas(windowWidth-20,windowHeight-20);
-  textSize(windowWidth/7)
-  clear();
-  
- 
+  textSize(windowWidth/7);
 }
 
-function drawMenu() {
+function showWinSize() {
+  console.log (windowWidth);
+  console.log (windowHeight);
+  
+}
+
+function txtSize() {
   
 }

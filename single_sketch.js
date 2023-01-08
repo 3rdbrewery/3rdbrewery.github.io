623d754cff6a5cc7778736a526fa4f309051d8bf
3rdbrewery <3rdbrewery@gmail.com>
2023-01-08 21:24:52 +0900

    commit

--- single_sketch.js
+++ single_sketch.js
@@ -3,20 +3,15 @@
 function setup (){
   createCanvas (windowWidth-20, windowHeight-20);
   textSize(windowWidth/7);
-  
 }
 
 function draw(){
   background(255);
   fill(0);
-  stroke(153);
+  noStroke();
   textAlign(LEFT, TOP);
   text('3rd_brewery', 0,0);
-  line(0,0,width,0);
-  text('3rd_brewery', 0,10 + windowHeight/7 );
-  line(0,10 + windowHeight/7,width,10 + windowHeight/7);
-  text('3rd_brewery', 0,10 + 2*windowHeight/7);
-  text('3rd_brewery', 0,10 + 3*windowHeight/7);
+  text('제3양조',0,300);
   showWinSize();
 }
 
